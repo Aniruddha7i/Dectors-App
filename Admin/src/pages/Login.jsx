15ecfd46c192bce808672d3a3d53405893c38ea2
Aniruddha Chandra <aniruddha.282003@gmail.com>
2025-02-07 16:14:21 +0530

    add admin control and backend

--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -1,24 +1,56 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets_admin/assets'
-const Login = () => {
+import React, { useState,useContext } from 'react'
+import { assets } from '../assets/assets_admin/assets';
+import { AdminContext } from '../context/AdminContext';
+import axios from 'axios';
 
+const Login = () => {
+    // state variable
     const [state, setState] = useState('Admin');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const {setAToken, backendUrl} = useContext(AdminContext);
+
+    const onSubmitHandler = async(event)=>{
+        event.preventDefault(); // prevent page reload
+
+        try{
+            if(state==='Admin'){
+                // we are in Admin state
+                const {data} = await axios.post(backendUrl+'/api/admin/login',{email,password})
+                if(data.success){
+                    console.log(data.atoken)
+                }
+            }else{
+                // we are in Doctor state
+
+            }
+
+        }catch(error){
+
+        }
+    }
 
     return (
-        <form className="min-h-[80vh] flex item-center">
+        <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex item-center">
             <div className='flex flex-col gap-3 m-auto item-start p-8 min-w-[340px] sm:min-w-96 border rounded-lg shadow-lg text-sm text-[#5E5E5E]'>
                 <p className='text-2xl font-semibold m-auto'> <span className='text-primary'>{state}</span> Login </p>
                 <div className='w-full'>
                     <p>Email:</p>
-                    <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
                 </div>
                 <div className='w-full'>
                     <p>Password:</p>
-                    <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+                    <input onChange={(e)=>setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
                 </div>
                 <button className='bg-primary text-white rounded-md p-2 text-base w-full'>Login</button>
-            </div>
+                {
+                    state === 'Admin'
+                        ? <p>Doctor Login? <span className='text-primary underline cursor-pointer' onClick={() => setState('Doctor')}>click here</span></p>
+                        : <p>Admin Login? <span className='text-primary underline cursor-pointer' onClick={() => setState('Admin')}>click here</span></p>
 
+                }
+            </div>
         </form>
     )
 }
